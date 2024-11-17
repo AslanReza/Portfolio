019942a9b4179648f1f6d11ede72a150dda2f732
Aslan <aslanreza77@gmail.com>
2024-11-17 13:28:57 +0330

    Set Route Pathes & Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { ThemeProvider } from "styled-components";
 import "./App.css";
 import GlobalStyle from "./BaseStyles";
-import { darkMode, lightMode } from "./components/Theme";
-
+import { lightMode } from "./components/Theme";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import SkillsPage from "./pages/SkillsPage";
+import BlogPage from "./pages/BlogPage";
+import WorkPage from "./pages/WorkPage";
+import { Route, Routes } from "react-router";
 function App() {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={darkMode}>App File</ThemeProvider>
+      <ThemeProvider theme={lightMode}>
+        <Routes>
+          <Route exact path="/" Component={HomePage} />
+          <Route exact path="/about" Component={AboutPage} />
+          <Route exact path="/blog" Component={BlogPage} />
+          <Route exact path="/work" Component={WorkPage} />
+          <Route exact path="/skills" Component={SkillsPage} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
