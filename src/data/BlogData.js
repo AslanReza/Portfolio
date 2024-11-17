b3075f7bce5c113b4047e466562bd7ffb7523cc0
Aslan <aslanreza77@gmail.com>
2024-11-17 10:42:12 +0330

    Setting Base Files & Assets Up

---
+++ src/data/BlogData.js
@@ -0,0 +1,18 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: "Understanding useMemo and useCallback",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc: "https://reactapp.ir/wp-content/uploads/reactjs.jpg",
+    link: "#",
+  },
+  {
+    id: 2,
+    name: "CSS in React Server Components",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc: "https://reactapp.ir/wp-content/uploads/reactjs.jpg",
+    link: "#",
+  },
+];
---
+++ src/data/WorkData.js
@@ -0,0 +1,18 @@
+export const Work = [
+    {
+        id:1,
+        name:"",
+        description:"",
+        tags:[""],
+        demo:"",
+        github:""
+    },
+    {
+        id:2,
+        name:"",
+        description:"",
+        tags:[""],
+        demo:"",
+        github:"" 
+    },
+] 
