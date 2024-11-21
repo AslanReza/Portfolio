126c974e9b314a88bb7fa97139ec5ecee1b669a0
Aslan <aslanreza77@gmail.com>
2024-11-21 11:34:35 +0330

    Add BlogComponent & Data to BlogPage

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -4,15 +4,53 @@ export const Blogs = [
     name: "Understanding useMemo and useCallback",
     tags: ["React"],
     date: "16 Nov, 2024",
-    imgSrc: "https://reactapp.ir/wp-content/uploads/reactjs.jpg",
-    link: "#",
+    imgSrc:
+      "https://images.unsplash.com/photo-1687603921109-46401b201195?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
   },
   {
     id: 2,
     name: "CSS in React Server Components",
     tags: ["React"],
     date: "16 Nov, 2024",
-    imgSrc: "https://reactapp.ir/wp-content/uploads/reactjs.jpg",
-    link: "#",
+    imgSrc:
+      "https://images.unsplash.com/photo-1670057046254-3b5095eb4b66?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
+  },
+  {
+    id: 3,
+    name: "CSS in React Server Components",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc:
+      "https://images.unsplash.com/photo-1670057046254-3b5095eb4b66?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
+  },
+  {
+    id: 4,
+    name: "CSS in React Server Components",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc:
+      "https://images.unsplash.com/photo-1670057046254-3b5095eb4b66?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
+  },
+  {
+    id: 5,
+    name: "CSS in React Server Components",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc:
+      "https://images.unsplash.com/photo-1670057046254-3b5095eb4b66?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
+  },
+  {
+    id: 6,
+    name: "CSS in React Server Components",
+    tags: ["React"],
+    date: "16 Nov, 2024",
+    imgSrc:
+      "https://images.unsplash.com/photo-1670057046254-3b5095eb4b66?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "",
   },
 ];
