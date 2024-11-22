aa6739c14f13cc197d8e122fa410c7611e005763
Aslan <aslanreza77@gmail.com>
2024-11-22 19:15:42 +0330

    Modify Big Title Component

--- src/components/subComponents/BigTitle.js
+++ src/components/subComponents/BigTitle.js
@@ -5,10 +5,10 @@ const Text = styled.h1`
   top: ${(props) => props.top};
   left: ${(props) => props.left};
   right: ${(props) => props.right};
-  color: ${(props) => `rgba(${props.theme.textRgba},0.1)`};
+  color: ${(props) => `rgba(${props.theme.textRgba},0.4)`};
   font-size: calc(5rem + 5vw);
   z-index: 0;
-  font-family: "Pacifico" !important;
+  font-family: "Ubuntu Mono" !important;
 `;
 const BigTitleBlog = (props) => {
   return (
